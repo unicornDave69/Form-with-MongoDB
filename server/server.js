28c6a02d85d324e546190399a7d1f15a77b9655d
David Dobrovolný <dejv.dobrovolny@seznam.cz>
2024-07-22 21:28:48 +0200

    last cahnges

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
+app.use(cors()); // Enable CORS
 app.use(bodyParser.json());
 
 mongoose
