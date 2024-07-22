dfc72db00494edc4eebe0b6b9d2f064043b8f487
David Dobrovolný <dejv.dobrovolny@seznam.cz>
2024-07-22 17:35:14 +0200

    first commit

--- server/server.js
+++ server/server.js
@@ -1,25 +1,54 @@
 const express = require("express");
-const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
 const app = express();
-const port = 5000;
+app.use(bodyParser.json());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+mongoose
+  .connect("mongodb://localhost:27017/tutorial", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
 
-const recordsController = require("./controller/records");
-const receiptsController = require("./controller/receipts");
-const activitiesController = require("./controller/activities");
-
-app.use(cors());
-
-app.use("/records", recordsController);
-app.use("/receipts", receiptsController);
-app.use("/activities", activitiesController);
-
-app.use((req, res, next) => {
-  res.status(404).send("404 Not Found");
+const userSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  contact: { type: String, required: true },
+  gender: { type: String, enum: ["Male", "Female", "Other"], required: true },
+  bestSubject: { type: [String], enum: ["English", "Math", "Physics"] },
+  resumeUrl: { type: String, required: true },
+  profileUrl: { type: String, required: true },
+  choice: { type: String, required: true },
+  about: { type: String, required: true },
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+const User = mongoose.model("User", userSchema);
+
+app.post("/users", async (req, res) => {
+  const user = new User({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    contact: req.body.contact,
+    gender: req.body.gender,
+    bestSubject: req.body.bestSubject,
+    resumeUrl: req.body.resumeUrl,
+    profileUrl: req.body.profileUrl,
+    choice: req.body.choice,
+    about: req.body.about,
+  });
+
+  try {
+    const result = await user.save();
+    res.send(result);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
 });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
